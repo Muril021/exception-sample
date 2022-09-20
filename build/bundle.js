9a6c2ac395e2c3d9d0e50dae2779a7e15ce619ee
Murilo Nascimento <murilojesus659@gmail.com>
2022-09-20 12:07:54 -0300

    Tratamento de erros TypeScript

---
+++ build/bundle.js
@@ -0,0 +1,22 @@
+var SystemError = /** @class */ (function () {
+    function SystemError(message, type) {
+        this.message = message;
+        this.type = type;
+    }
+    return SystemError;
+}());
+console.log('Será que vai dar erro?');
+try {
+    console.log('Antes do erro!');
+    throw new SystemError('Erro que eu mesmo gerei', 'Abstrato');
+    console.log('Depois do erro!');
+}
+catch (error) {
+    if (error instanceof SystemError) {
+        console.log("".concat(error.type, ": ").concat(error.message));
+    }
+    else {
+        console.log("Error: ".concat(error.message));
+    }
+}
+console.log('Acabou!');
